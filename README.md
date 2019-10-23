03010026efb9d9e4cdf8cc707d8284a9a5ba92b3
Fletcher Nichol <fnichol@nichol.ca>
2019-10-22 18:32:50 -0600

    Mortgage Munger: When your bank gives you garbage reports online
    Signed-off-by: Fletcher Nichol <fnichol@nichol.ca>

---
+++ README.md
@@ -0,0 +1,55 @@
+# Mortgage Munger
+
+This program takes multiple pastes to `stdin` of terrible Online Banking output report pages
+that look like this:
+
+```text
+Payment Date: 	Jul 20, 2018 	Jul 13, 2018 	Jul 6, 2018 	Jun 29, 2018 	Jun 22, 2018
+Principal Paid: 	$123.45		...
+Interest Paid: 	$67.89		...
+Taxes Paid: 	$12.34		...
+Total Paid: 	$567.89		...
+```
+
+and output a CSV file that looks like this:
+
+```text
+Payment Date,Principal Paid,Interest Paid,Taxes Paid,Total Paid
+2018-07-20,123.45,67.89,12.34,567.89
+...
+```
+
+## Usage
+
+You run the program:
+
+```console
+$ cargo run
+```
+
+and paste each report page in, followed with a newline and `Ctrl+D` to send an `eof` to the
+`stdin` stream. Repeat this process for each page and when finished, end with a single newline
+and `Ctrl+D`. The CSV report will be output to `stdout`.
+
+## Issues
+
+If you have any problems with or questions about this project, then I really do feel for you as you might have the same problems as this author. The author apologies.
+
+## Contributing
+
+I advise that you do not contribute to this project. It should need to exist and comes up every year this author does his year end taxes. Consider contributing to other projects maintained by the author or donate your time to a worthy cause.
+
+## Authors
+
+Angrily created and reluctantly maintained by [Fletcher Nichol][fnichol] (<fnichol@nichol.ca>).
+
+## License
+
+Licensed under the Mozilla Public License Version 2.0 ([LICENSE.txt][license]).
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the MPL-2.0 license, shall be
+licensed as above, without any additional terms or conditions.
+
+[fnichol]: https://github.com/fnichol
+[license]: https://github.com/fnichol/mortgage-munger/blob/master/LICENSE.txt
---
+++ src/main.rs
@@ -0,0 +1,102 @@
+use chrono::NaiveDate;
+use csv::{Writer, WriterBuilder};
+use std::error;
+use std::io::{self, Read};
+use std::result;
+
+/// Number of columns in each pasted report
+const INPUT_COLS: usize = 6;
+/// Number of rows in each pasted report
+const INPUT_ROWS: usize = 5;
+
+/// Program result type alias.
+type Result<T> = result::Result<T, Box<dyn error::Error>>;
+
+/// Program state.
+#[derive(Debug, PartialEq)]
+enum State {
+    /// No CSV title has been printed
+    NoTitle,
+    /// A CSV title has been printed
+    TitlePrinted,
+}
+
+fn main() -> Result<()> {
+    // Initialize state
+    let mut state = State::NoTitle;
+    // Create a CSV writer backed by a vec
+    let mut writer = WriterBuilder::new().from_writer(vec![]);
+    // Read in the pastes
+    read_pastes(&mut writer, &mut state)?;
+
+    // Write the final CSV to stdout
+    println!("{}", String::from_utf8(writer.into_inner()?)?);
+
+    Ok(())
+}
+
+/// Read a pasted report page on `stdin`.
+fn read_pastes(writer: &mut Writer<Vec<u8>>, state: &mut State) -> Result<()> {
+    loop {
+        // Read `stdin` to a buffer
+        let mut buf = String::new();
+        io::stdin().lock().read_to_string(&mut buf)?;
+        // If the buffer is empty, then we stop looping for input
+        if buf.trim().is_empty() {
+            break;
+        }
+
+        // Process the buffer
+        process_paste(buf, writer, state)?;
+    }
+
+    Ok(())
+}
+
+/// Process each pasted text buffer.
+fn process_paste(buf: String, writer: &mut Writer<Vec<u8>>, state: &mut State) -> Result<()> {
+    // Arrange all trimmed fields on all lines to be one linear vec
+    let input = buf
+        .lines()
+        .map(|line| line.split('\t').map(|col| col.trim()))
+        .flatten()
+        .collect::<Vec<_>>();
+
+    // Prepare an output vec that's initialized with values (required by `transpose`)
+    let mut output = Vec::with_capacity(input.len());
+    output.extend(std::iter::repeat("").take(input.len()));
+
+    // Transpose the "matrix", turning rows into columns
+    transpose::transpose(&input, &mut output, INPUT_COLS, INPUT_ROWS);
+
+    // Create an iterator of row fields to be re-output as lines
+    let mut lines = output.chunks(INPUT_ROWS);
+
+    // The a title line has not been written, add it to the CSV writer, and otherwise skip it
+    let titles = lines.next().expect("title line should exist");
+    if *state == State::NoTitle {
+        writer.write_record(titles.iter().map(|title| title.replace(":", "")))?;
+        *state = State::TitlePrinted;
+    }
+
+    for record in lines {
+        // Create an iterator over the output fields
+        let mut fields = record.iter();
+
+        // Parse the first field as a date and write out the only sane, unambiguous date format pls
+        let date = NaiveDate::parse_from_str(
+            fields.next().expect("should contain a date field"),
+            "%b %e, %Y",
+        )?;
+        writer.write_field(format!("{}", date))?;
+
+        // Strip the dollar sign off the currency amounts and write out all remaining fields
+        for field in fields {
+            writer.write_field(field.replace("$", ""))?;
+        }
+        // Write an empty row to signal the record line is terminated
+        writer.write_record(None::<&[u8]>)?;
+    }
+
+    Ok(())
+}
